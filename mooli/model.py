68d403c69c99b4117fed0fd3bf98dfbec2b54049
alshain <c.vonrueti@project-infcom.ch>
2011-02-17 14:47:48 +0100

    Cleans imports, adds ExternalPersonId.

--- mooli/model.py
+++ mooli/model.py
@@ -1,4 +1,5 @@
-from elixir import *
+import elixir
+
 
 # We would like to be able to create this schema in a specific database at
 # will, so we can test it easily.
@@ -8,118 +9,126 @@ from elixir import *
 __session__ = None
 
 
-class Language(Entity):
-    name = OneToMany("LanguageName")
+class Language(elixir.Entity):
+    name = elixir.OneToMany("LanguageName")
 
 
-class LanguageName(Entity):
-    translation_id = Field(Integer, primary_key=True)
-    language = ManyToOne('Language', primary_key=True)
-    text = Field(Unicode(20), unique=True)
+class LanguageName(elixir.Entity):
+    translation_id = elixir.Field(elixir.Integer, primary_key=True)
+    language = elixir.ManyToOne('Language', primary_key=True)
+    text = elixir.Field(elixir.Unicode(20), unique=True)
 
 
-class Provider(Entity):
-    url = Field(String(20), required=True, unique=True)
+class Provider(elixir.Entity):
+    url = elixir.Field(elixir.String(20), required=True, unique=True)
 
     def __init__(self, url):
         self.url = url
 
 
-class ExternalMovieId(Entity):
-    provider = ManyToOne('Provider')
-    value = Field(Unicode(40))
-    movie = ManyToOne('Movie')
+class ExternalMovieId(elixir.Entity):
+    provider = elixir.ManyToOne('Provider')
+    value = elixir.Field(elixir.Unicode(40))
+    movie = elixir.ManyToOne('Movie')
     # such as IMDB's, maybe some use text -> long
 
 
-class MovieTitle(Entity):
-    language = ManyToOne('Language')
-    movie = ManyToOne('Movie')
-    title = Field(UnicodeText)
+class MovieTitle(elixir.Entity):
+    language = elixir.ManyToOne('Language')
+    movie = elixir.ManyToOne('Movie')
+    title = elixir.Field(elixir.UnicodeText)
 
     def __init__(self, title, movie=None):
         self.title = unicode(title)
         self.movie = movie
 
-class Movie(Entity):
-    titles = OneToMany('MovieTitle', lazy=False)
-    plot = Field(UnicodeText)
-    year = Field(Integer)
-    tagline = Field(UnicodeText(255))
-    genres = ManyToMany('Genre')
-    identifiers = OneToMany('ExternalMovieId')
-    characters = ManyToMany('Character')
-    rating = OneToOne('Rating')
+class Movie(elixir.Entity):
+    titles = elixir.OneToMany('MovieTitle', lazy=False)
+    plot = elixir.Field(elixir.UnicodeText)
+    year = elixir.Field(elixir.Integer)
+    tagline = elixir.Field(elixir.UnicodeText(255))
+    genres = elixir.ManyToMany('Genre')
+    identifiers = elixir.OneToMany('ExternalMovieId')
+    characters = elixir.ManyToMany('Character')
+    rating = elixir.OneToOne('Rating')
 
     def __init__(self, title, year):
         title = unicode(title)
         self.titles.append(MovieTitle(title))
 
 
-class Character(Entity):
-    names = OneToMany('CharacterName')
-    movies = ManyToMany('Movie')
+class Character(elixir.Entity):
+    names = elixir.OneToMany('CharacterName')
+    movies = elixir.ManyToMany('Movie')
 
 
-class CharacterName(Entity):
-    language = ManyToOne('Language')
-    character = ManyToOne('Character')
+class CharacterName(elixir.Entity):
+    language = elixir.ManyToOne('Language')
+    character = elixir.ManyToOne('Character')
 
 
-class AssociatedPeople(Entity):
-    directors = ManyToMany('Person')
-    screenwriters = ManyToMany('Person')
-    actors = ManyToMany('Person')
+class AssociatedPeople(elixir.Entity):
+    directors = elixir.ManyToMany('Person')
+    screenwriters = elixir.ManyToMany('Person')
+    actors = elixir.ManyToMany('Person')
 
 
-class AspectRatio(Entity):
-    name = Field(Unicode(20), unique=True)
-    ratio = Field(Float)
-    movies = ManyToMany('Movie')
+class AspectRatio(elixir.Entity):
+    name = elixir.Field(elixir.Unicode(20), unique=True)
+    ratio = elixir.Field(elixir.Float)
+    movies = elixir.ManyToMany('Movie')
     # The Dark Knight has multiple...
 
 
-class Specification(Entity):
-    language = ManyToOne('Language')
-    color = Field(Boolean)
+class Specification(elixir.Entity):
+    language = elixir.ManyToOne('Language')
+    color = elixir.Field(elixir.Boolean)
 
 
-class Genre(Entity):
-    name = Field(UnicodeText(255))
+class Genre(elixir.Entity):
+    name = elixir.Field(elixir.UnicodeText(255))
 
 
-class Person(Entity):
-    name = Field(UnicodeText(50))
-    birthday = Field(Date)
+class Person(elixir.Entity):
+    name = elixir.Field(elixir.UnicodeText(50))
+    birthday = elixir.Field(elixir.Date)
+    identifiers = elixir.OneToMany('ExternalPersonId')
 
 
-class Rating(Entity):
+class ExternalPersonId(elixir.Entity):
+    provider = elixir.ManyToOne('Provider')
+    value = elixir.Field(elixir.Unicode(40))
+    person = elixir.ManyToOne('Person')
+    # such as IMDB's, maybe some use text -> long
+
+
+class Rating(elixir.Entity):
     """Score of a movie as users have voted."""
-    name = Field(UnicodeText(40))
-    rating = Field(Float)
-    # OneToOne must have a ManyToOne as inverse
-    movie = ManyToOne('Movie')
-    votes = Field(Integer)
+    name = elixir.Field(elixir.UnicodeText(40))
+    rating = elixir.Field(elixir.Float)
+    # elixir.OneToOne must have a elixir.ManyToOne as inverse
+    movie = elixir.ManyToOne('Movie')
+    votes = elixir.Field(elixir.Integer)
 
     def __cmp__(self, other):
-        if self.position < other.position:
+        if self.rating < other.rating:
             return -1
-        elif self.primary_key > other.position:
+        elif self.rating > other.rating:
             return 1
         else:
             return 0
 
 
-class AgeRating(Entity):
+class AgeRating(elixir.Entity):
     """MPAA rating."""
-    name = Field(UnicodeText(40))
-    position = Field(Float)
-    movies = ManyToMany('Movie')
+    name = elixir.Field(elixir.UnicodeText(40))
+    position = elixir.Field(elixir.Float)
+    movies = elixir.ManyToMany('Movie')
 
     def __cmp__(self, other):
         if self.position < other.position:
             return -1
-        elif self.primary_key > other.position:
+        elif self.position > other.position:
             return 1
         else:
             return 0
