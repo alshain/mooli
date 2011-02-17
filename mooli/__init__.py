8a2976b30dc24a777b796f509c296f0e4a54cc5f
alshain <c.vonrueti@project-infcom.ch>
2011-02-17 17:06:32 +0100

    Adds MooliError..

--- mooli/__init__.py
+++ mooli/__init__.py
@@ -3,6 +3,11 @@
 from sqlalchemy import create_engine
 from mooli.library import Library
 
+
+class MooliError(Exception):
+    pass
+
+
 def open(path):
     """Open a mooli library at said path."""
     engine = create_engine("sqlite:///%s" % path)
