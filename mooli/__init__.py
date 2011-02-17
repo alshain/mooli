e586b94c0caf7e856b4f6518e2e38bd058bd194f
alshain <c.vonrueti@project-infcom.ch>
2011-02-17 17:07:11 +0100

    Adds connect function to mooli __init__.py

--- mooli/__init__.py
+++ mooli/__init__.py
@@ -11,4 +11,7 @@ class MooliError(Exception):
 def open(path):
     """Open a mooli library at said path."""
     engine = create_engine("sqlite:///%s" % path)
+    return connect(engine)
+
+def connect(engine):
     return Library(engine)
